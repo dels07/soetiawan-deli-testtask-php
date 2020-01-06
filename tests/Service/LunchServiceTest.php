22f23999c1bd7ecb73066d6c202f872c039dd379
dels07 <deli.soetiawan@gmail.com>
2020-01-06 22:37:37 +0700

    feat(lunch): add initial lunch service test

---
+++ tests/Service/LunchServiceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+
+class LunchServiceTest extends TestCase
+{
+    public function test_able_to_get_lunch_recipes()
+    {
+        $this->assertTrue(true);
+    }
+}
